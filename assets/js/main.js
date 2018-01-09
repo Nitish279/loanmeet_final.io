8b4a1e02e87e4bd5268546f4a69a95b094d2de40
nitish67 <nitish4u029@gmail.com>
2018-01-09 19:25:38 +0530

    Loanmeet final commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    
+    $('ul.nav li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    });
+
+
+
+    function toggleIcon(e) {
+        $(e.target)
+            .prev('.panel-heading')
+            .find(".more-less")
+            .toggleClass('glyphicon-plus glyphicon-minus');
+    }
+    $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+    $('.panel-group').on('shown.bs.collapse', toggleIcon);
+
+});
+
